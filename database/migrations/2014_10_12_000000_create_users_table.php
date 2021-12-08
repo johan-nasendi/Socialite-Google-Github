552b172aba42d6b840048b3a4fc69aceedb1132b
Johan <57840332+johannasendi@users.noreply.github.com>
2021-12-08 23:13:59 +0900

    update By Johan
    composer require santigarcor/laratrust
    
    for users role, test with social media auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('username');
-            $table->string('avatar');
+            $table->string('avatar')->nullable();
             $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
